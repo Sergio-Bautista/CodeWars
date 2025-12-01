7dcec5268d8286238347c7b1a999f4088673ffcf
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2025-11-30 19:00:04 -0600

    Problem Solution

---
+++ 7kyu/facotiral.js
@@ -0,0 +1,13 @@
+/*
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+*/
+function factorial(n){
+  //your code here
+  let result = 1;
+  for (let i =1; i<=n; i++){
+    result *= i
+  }
+  return result
+}
