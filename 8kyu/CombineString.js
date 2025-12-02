f5e5a4c970b119d5bd8cf4e8cd349deb0ba75610
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2025-12-01 21:44:04 -0600

    Problem Solution

---
+++ 8kyu/CombineString.js
@@ -0,0 +1,11 @@
+// Combine strings function
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+// Create the combineNames function here
+function combineNames(fn, ln){
+    return `${fn} ${ln}`
+}
