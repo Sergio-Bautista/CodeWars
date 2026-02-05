b136082310631cbb4270f6bd30adffdcf7f8e08a
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2026-02-04 23:10:02 -0600

    Problem Solution

---
+++ 7kyu/Switchheroo.js
@@ -0,0 +1,22 @@
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+function switcheroo(x){
+  let switched = [];
+  x.split("").forEach(el =>{
+    if(el === 'a'){
+      switched.push('b')
+    }else if(el === 'b'){
+      switched.push('a')
+    }else{
+      switched.push(el)
+    }
+  })
+  return switched.join('')
+}
